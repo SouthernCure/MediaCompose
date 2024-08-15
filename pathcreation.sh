f685af0bd7df6a59495bbc08368419f382e38d50
SouthernCure <mekhigh10@gmail.com>
2024-08-15 13:28:14 -0400

    Changed to home dir

--- pathcreation.sh
+++ pathcreation.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-sudo mkdir -p /mnt/mediaserver
-sudo mkdir -p /mnt/mediaserver/data
+sudo mkdir -p /home/user
+sudo mkdir -p /home/user/data
 
-sudo mkdir -p /mnt/mediaserver/data/arrs
-sudo mkdir -p /mnt/mediaserver/data/arrs/homarr
-sudo mkdir -p /mnt/mediaserver/data/arrs/homarr/configs
-sudo mkdir -p /mnt/mediaserver/data/arrs/homarr/data
-sudo mkdir -p /mnt/mediaserver/data/arrs/homarr/icons
-sudo mkdir -p /mnt/mediaserver/data/arrs/radarr
-sudo mkdir -p /mnt/mediaserver/data/arrs/radarr/configs
-sudo mkdir -p /mnt/mediaserver/data/arrs/sonarr
-sudo mkdir -p /mnt/mediaserver/data/arrs/sonarr/configs
-sudo mkdir -p /mnt/mediaserver/data/arrs/jellyseerr
-sudo mkdir -p /mnt/mediaserver/data/arrs/jellyseerr/configs
+sudo mkdir -p /home/user/data/arrs
+sudo mkdir -p /home/user/data/arrs/homarr
+sudo mkdir -p /home/user/data/arrs/homarr/configs
+sudo mkdir -p /home/user/data/arrs/homarr/data
+sudo mkdir -p /home/user/data/arrs/homarr/icons
+sudo mkdir -p /home/user/data/arrs/radarr
+sudo mkdir -p /home/user/data/arrs/radarr/configs
+sudo mkdir -p /home/user/data/arrs/sonarr
+sudo mkdir -p /home/user/data/arrs/sonarr/configs
+sudo mkdir -p /home/user/data/arrs/jellyseerr
+sudo mkdir -p /home/user/data/arrs/jellyseerr/configs
 
-sudo mkdir -p /mnt/mediaserver/data/torrents
-sudo mkdir -p /mnt/mediaserver/data/torrents/books
-sudo mkdir -p /mnt/mediaserver/data/torrents/movies
-sudo mkdir -p /mnt/mediaserver/data/torrents/music
-sudo mkdir -p /mnt/mediaserver/data/torrents/tv
+sudo mkdir -p /home/user/data/torrents
+sudo mkdir -p /home/user/data/torrents/books
+sudo mkdir -p /home/user/data/torrents/movies
+sudo mkdir -p /home/user/data/torrents/music
+sudo mkdir -p /home/user/data/torrents/tv
 
-sudo mkdir -p /mnt/mediaserver/data/usenet
-sudo mkdir -p /mnt/mediaserver/data/usenet/incomplete
-sudo mkdir -p /mnt/mediaserver/data/usenet/complete
-sudo mkdir -p /mnt/mediaserver/data/usenet/complete/books
-sudo mkdir -p /mnt/mediaserver/data/usenet/complete/movies
-sudo mkdir -p /mnt/mediaserver/data/usenet/complete/music
-sudo mkdir -p /mnt/mediaserver/data/usenet/complete/tv
+sudo mkdir -p /home/user/data/usenet
+sudo mkdir -p /home/user/data/usenet/incomplete
+sudo mkdir -p /home/user/data/usenet/complete
+sudo mkdir -p /home/user/data/usenet/complete/books
+sudo mkdir -p /home/user/data/usenet/complete/movies
+sudo mkdir -p /home/user/data/usenet/complete/music
+sudo mkdir -p /home/user/data/usenet/complete/tv
 
-sudo mkdir -p /mnt/mediaserver/data/media
-sudo mkdir -p /mnt/mediaserver/data/media/books
-sudo mkdir -p /mnt/mediaserver/data/media/movies
-sudo mkdir -p /mnt/mediaserver/data/media/music
-sudo mkdir -p /mnt/mediaserver/data/media/tv
+sudo mkdir -p /home/user/data/media
+sudo mkdir -p /home/user/data/media/books
+sudo mkdir -p /home/user/data/media/movies
+sudo mkdir -p /home/user/data/media/music
+sudo mkdir -p /home/user/data/media/tv
