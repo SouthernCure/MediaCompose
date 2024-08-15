8f4f45564c7f28d3d7c539a9a0ab1fe08cb4764c
SouthernCure <mekhigh10@gmail.com>
2024-08-15 13:30:35 -0400

    Added User variables

--- pathcreation.sh
+++ pathcreation.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-sudo mkdir -p /home/user
-sudo mkdir -p /home/user/data
+sudo mkdir -p /home/$USER
+sudo mkdir -p /home/$USER/data
 
-sudo mkdir -p /home/user/data/arrs
-sudo mkdir -p /home/user/data/arrs/homarr
-sudo mkdir -p /home/user/data/arrs/homarr/configs
-sudo mkdir -p /home/user/data/arrs/homarr/data
-sudo mkdir -p /home/user/data/arrs/homarr/icons
-sudo mkdir -p /home/user/data/arrs/radarr
-sudo mkdir -p /home/user/data/arrs/radarr/configs
-sudo mkdir -p /home/user/data/arrs/sonarr
-sudo mkdir -p /home/user/data/arrs/sonarr/configs
-sudo mkdir -p /home/user/data/arrs/jellyseerr
-sudo mkdir -p /home/user/data/arrs/jellyseerr/configs
+sudo mkdir -p /home/$USER/data/arrs
+sudo mkdir -p /home/$USER/data/arrs/homarr
+sudo mkdir -p /home/$USER/data/arrs/homarr/configs
+sudo mkdir -p /home/$USER/data/arrs/homarr/data
+sudo mkdir -p /home/$USER/data/arrs/homarr/icons
+sudo mkdir -p /home/$USER/data/arrs/radarr
+sudo mkdir -p /home/$USER/data/arrs/radarr/configs
+sudo mkdir -p /home/$USER/data/arrs/sonarr
+sudo mkdir -p /home/$USER/data/arrs/sonarr/configs
+sudo mkdir -p /home/$USER/data/arrs/jellyseerr
+sudo mkdir -p /home/$USER/data/arrs/jellyseerr/configs
 
-sudo mkdir -p /home/user/data/torrents
-sudo mkdir -p /home/user/data/torrents/books
-sudo mkdir -p /home/user/data/torrents/movies
-sudo mkdir -p /home/user/data/torrents/music
-sudo mkdir -p /home/user/data/torrents/tv
+sudo mkdir -p /home/$USER/data/torrents
+sudo mkdir -p /home/$USER/data/torrents/books
+sudo mkdir -p /home/$USER/data/torrents/movies
+sudo mkdir -p /home/$USER/data/torrents/music
+sudo mkdir -p /home/$USER/data/torrents/tv
 
-sudo mkdir -p /home/user/data/usenet
-sudo mkdir -p /home/user/data/usenet/incomplete
-sudo mkdir -p /home/user/data/usenet/complete
-sudo mkdir -p /home/user/data/usenet/complete/books
-sudo mkdir -p /home/user/data/usenet/complete/movies
-sudo mkdir -p /home/user/data/usenet/complete/music
-sudo mkdir -p /home/user/data/usenet/complete/tv
+sudo mkdir -p /home/$USER/data/usenet
+sudo mkdir -p /home/$USER/data/usenet/incomplete
+sudo mkdir -p /home/$USER/data/usenet/complete
+sudo mkdir -p /home/$USER/data/usenet/complete/books
+sudo mkdir -p /home/$USER/data/usenet/complete/movies
+sudo mkdir -p /home/$USER/data/usenet/complete/music
+sudo mkdir -p /home/$USER/data/usenet/complete/tv
 
-sudo mkdir -p /home/user/data/media
-sudo mkdir -p /home/user/data/media/books
-sudo mkdir -p /home/user/data/media/movies
-sudo mkdir -p /home/user/data/media/music
-sudo mkdir -p /home/user/data/media/tv
+sudo mkdir -p /home/$USER/data/media
+sudo mkdir -p /home/$USER/data/media/books
+sudo mkdir -p /home/$USER/data/media/movies
+sudo mkdir -p /home/$USER/data/media/music
+sudo mkdir -p /home/$USER/data/media/tv
 
-sudo chown -R $USER:$USER /home/user/data
-sudo chmod -R a=,a+rX,u+w,g+w /home/user/data
+sudo chown -R $$USER:$$USER /home/$USER/data
+sudo chmod -R a=,a+rX,u+w,g+w /home/$USER/data
