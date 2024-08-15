a4ca45585cd14e261cc5615d7ea00e7f639b1e41
SouthernCure <mekhigh10@gmail.com>
2024-08-15 00:54:59 -0400

    Added path ideas

---
+++ pathcreation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p /mnt/mediaserver
+mkdir -p /mnt/mediaserver/data
+
+mkdir -p /mnt/mediaserver/data/torrents
+mkdir -p /mnt/mediaserver/data/torrents/books
+mkdir -p /mnt/mediaserver/data/torrents/movies
+mkdir -p /mnt/mediaserver/data/torrents/music
+mkdir -p /mnt/mediaserver/data/torrents/tv
+
+mkdir -p /mnt/mediaserver/data/usenet
+mkdir -p /mnt/mediaserver/data/usenet/incomplete
+mkdir -p /mnt/mediaserver/data/usenet/complete
+mkdir -p /mnt/mediaserver/data/usenet/complete/books
+mkdir -p /mnt/mediaserver/data/usenet/complete/movies
+mkdir -p /mnt/mediaserver/data/usenet/complete/music
+mkdir -p /mnt/mediaserver/data/usenet/complete/tv
+
+mkdir -p /mnt/mediaserver/data/media
+mkdir -p /mnt/mediaserver/data/media/books
+mkdir -p /mnt/mediaserver/data/media/movies
+mkdir -p /mnt/mediaserver/data/media/music
+mkdir -p /mnt/mediaserver/data/media/tv
