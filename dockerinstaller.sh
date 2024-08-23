6f0a11bd92b757891b25ad251497f769637be7e8
SouthernCure <121195649+SouthernCure@users.noreply.github.com>
2024-08-22 23:06:36 -0400

    Update dockerinstaller.sh
    Fixed for raspberry pi

--- dockerinstaller.sh
+++ dockerinstaller.sh
@@ -10,6 +10,6 @@ sudo chmod 666 /var/run/docker.sock
 docker ps
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
